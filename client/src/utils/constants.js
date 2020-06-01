ad0bc7e4d474d4ac98c5a34ed308b9067a22c0b8
StevenLiao123 <182272546@qq.com>
2020-06-01 14:33:55 +1000

    Correct the MongoDB URL

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -2,5 +2,5 @@
     The moudle to contain some constants
 */
 
-export const MONGO_BASE_URL = process.env.NODE_ENV === "production" ? "https://shrouded-tor-98527.herokuapp.com//api" : "http://localhost:8080/api"; // the base url of MongoDB
+export const MONGO_BASE_URL = process.env.NODE_ENV === "production" ? "https://shrouded-tor-98527.herokuapp.com/api" : "http://localhost:8080/api"; // the base url of MongoDB
 export const AZURE_BASE_URL = "http://dmmw-api.australiaeast.cloudapp.azure.com:8080"; // the url of Azure
